59ec1cc64826a6d7858546a30bb300d08665adf2
Pranav Jain <pranav19sept@gmail.com>
2021-09-03 21:45:13 +0530

    Added bill, event and user apps Updated settings.py

--- blitter/settings.py
+++ blitter/settings.py
@@ -51,9 +51,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps
+    'bill',
+    'event',
+    'user',
+    # Libraries
+    'corsheaders',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
