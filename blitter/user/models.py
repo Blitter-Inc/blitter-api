66b54e0705acb5f341ae2d690234e447ea299bc7
Pranav Jain <pranav19sept@gmail.com>
2021-09-06 14:16:48 +0530

    Added TimstampMixin to User model Modified email and bio field Updated migrations

--- blitter/user/models.py
+++ blitter/user/models.py
@@ -2,15 +2,16 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 
+from blitter.shared.models import TimestampMixin
 from .manager import UserManager
 
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(TimestampMixin, AbstractBaseUser, PermissionsMixin):
 
     name = models.CharField('Name', max_length=254, blank=False)
     phone = models.CharField('Phone', max_length=18, blank=False, unique=True)
-    email = models.EmailField('Email', blank=False, null=True, unique=True)
-    bio = models.TextField('Bio', blank=True, null=True)
+    email = models.EmailField('Email', blank=True, null=True, unique=True)
+    bio = models.TextField('Bio', blank=True)
     avatar = models.FileField(
         'Avatar', upload_to='media/user/avatar', blank=True, null=True)
     is_staff = models.BooleanField('Is staff', default=False)
