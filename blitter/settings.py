676e5431bf8d91be46745b74b8f458ddda09e568
Pranav Jain <pranav19sept@gmail.com>
2022-01-30 13:38:07 +0530

    django_stubs_ext required only if server runs in DEBUG mode

--- blitter/settings.py
+++ blitter/settings.py
@@ -14,9 +14,6 @@ import os
 from datetime import timedelta
 from dotenv import dotenv_values
 from pathlib import Path
-import django_stubs_ext
-
-django_stubs_ext.monkeypatch()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -200,3 +197,7 @@ if DEBUG and ENABLE_SQL_LOGGING:
             },
         },
     }
+
+if DEBUG:
+    import django_stubs_ext
+    django_stubs_ext.monkeypatch()
